fe6a0168f8d9793882ce9aa8a4208a2f27a61a4d
Your Name <you@example.com>
2019-08-28 10:52:26 -0300

    mudanca2

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333/api"
-  //process.env.REACT_APP_API_URL
+  /*"http://localhost:3333/api"*/
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
